cb81c93deb04ae04e7cf79d82642626594baee5d
bak0903 <bak0903@mail.ru>
2019-02-28 12:56:43 +0600

    Добавил контейнер для главной страницы Main(компонента и стили)

---
+++ src/containers/Main/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './Main.css';
+
+
+class Main extends Component {
+    render() {
+        return <div className='Main'>
+            <h1>Welcom to GameZone!</h1>
+            <p>Please, choose one in sidebar.</p>
+        </div>;
+    }
+}
+
+
+export default Main;
