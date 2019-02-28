3ca56f4a10501b64e16e9b4b1f0ce8250eb1605a
bak0903 <bak0903@mail.ru>
2019-02-28 12:59:21 +0600

    Подключил React-Router, добавил Layout и Main в App.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Lotto from './containers/Lotto/Lotto';
 import Boxes from './containers/Boxes/Boxes';
-
+import Layout from "./Layout";
+import Main from './containers/Main/Main';
 
 class App extends Component {
 
-  render() {
-    return (
-        <div>
-            <Lotto/>
-            <Boxes/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className={"App"}>
+                <BrowserRouter>
+                    <Layout>
+                        <Switch>
+                            <Route path="/lotto" component={Lotto}/>
+                            <Route path="/boxes" exact component={Boxes}/>
+                            <Route path="/" component={Main}/>
+                        </Switch>
+                    </Layout>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
